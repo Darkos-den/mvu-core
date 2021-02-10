b5261780098cf8997b7e89037008f4f321fa23a2
Denis <loboda.deni@gmail.com>
2021-02-10 20:18:17 +0300

    configure publishing

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,15 +1,21 @@
+import org.jetbrains.kotlin.konan.properties.loadProperties
+import kotlin.io.println
+
 plugins {
     id("com.android.library")
     kotlin("multiplatform")
     `maven-publish`
+    id("com.jfrog.artifactory")
 }
 
-val organization = "darkosinc"
-val repository = "MVU"
+val organization = "darkos"
+val repository = "mvu"
 
 val artifactName = "core"
 val artifactGroup = "com.$organization.$repository"
 val artifactVersion = "1.0.0-rc1"
+
+val repoName = "mvu-core"
 
 group = artifactGroup
 version = artifactVersion
@@ -37,9 +43,6 @@ android {
     }
 }
 
-dependencies {
-}
-
 kotlin {
     android("android") {
         publishAllLibraryVariants()
@@ -59,5 +62,74 @@ kotlin {
         }
         val androidMain by getting
         val iosMain by getting
+    }
+}
+
+val localPropsFile: File = project.rootProject.file("local.properties")
+val localProperties = loadProperties(localPropsFile.absolutePath)
+
+val mUsername: String? by localProperties
+val mPassword: String? by localProperties
+
+artifactory {
+    val baseUrl = "https://darkos.jfrog.io/artifactory"
+
+    setContextUrl(baseUrl)   //The base Artifactory URL if not overridden by the publisher/resolver
+
+    publish(closureOf<org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig> {
+        setContextUrl(baseUrl)
+
+        repository(closureOf<org.jfrog.gradle.plugin.artifactory.dsl.DoubleDelegateWrapper> {
+            invokeMethod("setRepoKey", repoName)
+            invokeMethod("setUsername", mUsername)
+            invokeMethod("setPassword", mPassword)
+        })
+    })
+
+    resolve(closureOf<org.jfrog.gradle.plugin.artifactory.dsl.ResolverConfig> {
+        repository(closureOf<org.jfrog.gradle.plugin.artifactory.dsl.DoubleDelegateWrapper> {
+            invokeMethod("setRepoKey", "mvu")
+            invokeMethod("setUsername", mUsername)
+            invokeMethod("setPassword", mPassword)
+            invokeMethod("setMaven", true)
+        })
+    })
+}
+
+publishing {
+    val vcs = "https://darkos.jfrog.io/artifactory/$repoName/"
+
+    publications.filterIsInstance<MavenPublication>().forEach { publication ->
+        println(publication.name)
+
+        publication.pom {
+            name.set(artifactName)
+            description.set(project.description)
+            url.set(vcs)
+
+            licenses {
+                license {
+                    name.set("The Apache Software License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    distribution.set("repo")
+                }
+            }
+            scm {
+                url.set(vcs)
+                tag.set(project.version.toString())
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "artifactory"
+            url =
+                uri("https://darkos.jfrog.io/artifactory/$repoName/")
+            credentials {
+                username = mUsername
+                password = mPassword
+            }
+        }
     }
 }
